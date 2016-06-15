feef9e40600b5c7a4ef01e2ce5eccd6914b5a937
Lucas Cantor <lucascantor@gmail.com>
2016-06-15 10:24:37 -0700

    updated comment

--- deadmansswitch.sh
+++ deadmansswitch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Return whether .dead_mans_switch has been touched within the past 24 hours
+# Shut down if ~/.dead_mans_switch not touched within past 24 hours
 
 if [ $(find /Users/lucas/ -mtime -1 -type f -name ".dead_mans_switch" 2>/dev/null) ]
 then

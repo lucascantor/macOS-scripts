a0dfcc34065429623d3f53dbd3055ed7ff307a71
Lucas Cantor <lucascantor@gmail.com>
2016-06-15 10:23:22 -0700

    added code to shut down

--- deadmansswitch.sh
+++ deadmansswitch.sh
@@ -6,7 +6,8 @@ if [ $(find /Users/lucas/ -mtime -1 -type f -name ".dead_mans_switch" 2>/dev/nul
 then
 	echo "Dead mans switch touched today."
 else
-	echo "Dead mans switch NOT touched today."
+	echo "Dead mans switch NOT touched today. Shutting down now."
+	/sbin/shutdown now
 fi
 
 exit 0
